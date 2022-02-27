5ab412e5c5db689fcc600063f560f01fe616231e
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-27 20:38:17 -0300

    component header (clip)

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-import { Container, Content } from "./styles";
+import {
+  Container,
+  Content,
+  HeaderWrapper,
+  Header,
+  DropboxLogo,
+} from "./styles";
 
 interface Props {
   variant: "blue" | "beige" | "white" | "black";
@@ -9,8 +15,18 @@ interface Props {
 }
 
 const Section: React.FC<Props> = ({ variant, title, description }) => {
+  const buttonVariant = Math.round(Math.random());
   return (
     <Container className={variant}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo />
+            <span>Dropbox</span>
+          </h1>
+          <button>{buttonVariant === 0 ? "Acessar" : "Interagir"}</button>
+        </Header>
+      </HeaderWrapper>
       <Content>
         <h2>{title}</h2>
         <p>{description}</p>
--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { FaDropbox } from "react-icons/fa";
+
 export const Container = styled.div`
   --padding-top: 100px;
   --padding-bottom: 120px;
@@ -36,6 +38,7 @@ export const Container = styled.div`
     }
   }
   background: var(--bg-color);
+  position: relative;
 `;
 export const Content = styled.div`
   z-index: 2;
@@ -56,4 +59,54 @@ export const Content = styled.div`
   }
 
   padding: var(--padding-top) 32px var(--padding-bottom);
+`;
+
+export const HeaderWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  clip: rect(auto, auto, auto, auto)
+`;
+
+export const Header = styled.header`
+  z-index: 3;
+  background: var(--bg-color);
+  display: flex;
+  justify-content: space-between;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 16px 32px;
+  > h1 {
+    display: flex;
+    align-items: center;
+    > span {
+      color: var(--text-color);
+      margin-left: 10px;
+      font-size: 29px;
+    }
+  }
+  > button {
+    color: var(--text-color);
+    background: none;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+export const DropboxLogo = styled(FaDropbox)`
+  width: 36px;
+  height: 32px;
+  fill: var(--logo-color);
 `;
