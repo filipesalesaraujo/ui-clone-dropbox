67a727ab22ceaaa47068aab5bf664f05229518e2
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-27 20:04:16 -0300

    estilizacao inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
-  return <h1>ui-clone-dropbox</h1>;
+  return (
+    <>
+      <h1>ui-clone-dropbox</h1>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
